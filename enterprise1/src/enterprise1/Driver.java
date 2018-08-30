e92f83cc9204e0c3c97e87e9f617b4b8c235663c
tchilds26 <tylerchilds23@gmail.com>
2018-08-30 17:44:27 -0400

    uiAddedNoListeners

--- enterprise1/src/enterprise1/Driver.java
+++ enterprise1/src/enterprise1/Driver.java
@@ -11,10 +11,9 @@ import javax.swing.ImageIcon;
 public class Driver{
 public static void main(String args[]) {
 ////	
-EnterpriseGui start = new EnterpriseGui();
 EnterpriseGui labelFrame = new EnterpriseGui();
 labelFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-labelFrame.setSize( 850, 400 );
+labelFrame.setSize( 800, 400 );
 labelFrame.setVisible( true );
 ////
 
--- enterprise1/src/enterprise1/EnterpriseGui.java
+++ enterprise1/src/enterprise1/EnterpriseGui.java
@@ -15,20 +15,63 @@ import javax.swing.JTextField;
 public class EnterpriseGui extends JFrame{
 	private JTextField bookquantity;
 	private JLabel label2;
+	private JTextField bookID;
+	private JLabel bookIDLabel;
+	private JTextField bookQuantity;
+	private JLabel bookQuantityLabel;
+	
+	private JLabel ItemInfo;
+	private JTextField ItemInfoBox;
+	private JLabel OrderSubtotal;
+	private JTextField OrderSubtotalBox;
+	
+	private JButton submitButton;
+	private JButton orderButton;
+	private JButton historyButton;
+	private JButton otherButton;
+	
 	
 	public EnterpriseGui()
 		{
-			super("Tylers Book Store");
+			super("Tyler's Book Store");
 			setLayout(new FlowLayout());
 			
 			label2 = new JLabel("Enter quantity of books:");
 			add(label2);
-			
 			bookquantity = new JTextField(50);
 			add(bookquantity);
 			
+			bookIDLabel = new JLabel("Enter Book ID for Item # ");
+			add(bookIDLabel);
+			bookID = new JTextField(50);
+			add(bookID);
+			
+			bookQuantityLabel = new JLabel("Enter quantity for Item #");
+			add(bookQuantityLabel);
+			bookQuantity = new JTextField(50);
+			add(bookQuantity);
 		
-
+			ItemInfo = new JLabel("Item #" + "Info");
+			add(ItemInfo);
+			ItemInfoBox = new JTextField(50);
+			ItemInfoBox.setHorizontalAlignment(JTextField.RIGHT);
+			add(ItemInfoBox);
+			ItemInfoBox.setEditable(false);
+			
+			OrderSubtotal = new JLabel("OrderSubtotal for " + "items");
+			add(OrderSubtotal);
+			OrderSubtotalBox = new JTextField(50);
+			add(OrderSubtotalBox);
+			OrderSubtotalBox.setEditable(false);
+			
+			submitButton = new JButton("Submit");
+			add(submitButton);
+			orderButton = new JButton("Order");
+			add(orderButton);
+			historyButton = new JButton("History");
+			add(historyButton);
+			otherButton = new JButton("Other");
+			add(otherButton);
 		}
 }
 
